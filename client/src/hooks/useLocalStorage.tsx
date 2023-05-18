316ac957d1a8b5fce3e3831f9079ae27786970ea
michaeland974 <micanderson29@gmail.com>
2023-05-17 21:43:25 -0500

    feat: useNotes hook updated with local storage

--- client/src/hooks/useLocalStorage.tsx
+++ client/src/hooks/useLocalStorage.tsx
@@ -1,17 +1,22 @@
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { Time } from '../Components/Stopwatch/interfaces';
+
+type StorageKeys = {
+  storedTime: string,
+  storedNotes: string
+}
 
 const getStoredValue = (key: string): string|undefined => {
   return key ? localStorage.getItem(key) as string : undefined;
 };
 
-export const useLocalStorage = (key: string , dep: Time) => {
+export const useLocalStorage = (key: keyof StorageKeys, dep: Time | any) => {
   const [value, setValue] = useState(() => {
     return getStoredValue(key);
   });
-
+ 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(dep.state));
+    localStorage.setItem(key, JSON.stringify(dep.state ?? dep));
 }, [dep]);
 
   return [value, setValue];
--- client/src/hooks/useNotes.tsx
+++ client/src/hooks/useNotes.tsx
@@ -1,9 +1,10 @@
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
+import { useLocalStorage } from './useLocalStorage';
 
 export const useNotes = () => {
   const [input, setInput] = useState('');
   const [list, setList] = useState<string[]>([]);
-
+  const [localStorageNotes] = useLocalStorage('storedNotes', list);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
   };
@@ -24,6 +25,12 @@ export const useNotes = () => {
     setList(editedList);
   };
   
+  useEffect(() => {
+    console.log(localStorageNotes);
+    const localNotes: string[] = JSON.parse(localStorageNotes as string);
+    setList(localNotes);
+  }, []);
+  
   return [{input, list, setList, 
-           handleChange, handleAddNote, editNote}]; //as const;
+           handleChange, handleAddNote, editNote}];
 };
