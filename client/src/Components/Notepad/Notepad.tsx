93d8474a7c9fe309b3abb3e439dc80bfe7c7e0fe
michaeland974 <micanderson29@gmail.com>
2023-05-23 23:59:18 -0500

    style: Mobile styles updated for Notepad component

--- client/src/Components/Notepad/Notepad.tsx
+++ client/src/Components/Notepad/Notepad.tsx
@@ -6,15 +6,26 @@ import { useNotes } from '../../hooks/useNotes';
 export const Notepad = () => {
   const [{input, list, setList, 
           handleChange, handleAddNote, editNote}] = useNotes();
+  const isEmpty = (list.length === 0);
+
+  const Empty = () => {
+    return(
+      <div id={styles['empty']}>
+        {[...Array(5)].map((el) => <span className={styles['line']}></span>)}
+      </div>
+    );
+  };
 
   return (
     <div className={styles['container']}>
       <Input value={input}
              actions={{change: handleChange,
                        add: handleAddNote}}/>
-      <List notes={list}
-            actions={{set: setList, 
-                      edit: editNote}}/>
+      {isEmpty ? <Empty /> : 
+                 <List notes={list}
+                    actions={{set: setList, 
+                              edit: editNote}}/>}
+      
     </div>
   );  
 };
